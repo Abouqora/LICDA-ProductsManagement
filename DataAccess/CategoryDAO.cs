8fad65570847df42b39ede5e4ebdbb7f4651957e
Y.Abouqora <yabouqora@gmail.com>
2023-04-08 13:16:46 +0000

    Add files via upload

---
+++ DataAccess/CategoryDAO.cs
@@ -0,0 +1,33 @@
+﻿using BusinessObjects;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class CategoryDAO
+    {
+        public static List<Category> GetCategories() { 
+
+            var listCategories = new List<Category>();
+            try {
+                using (var context = new MyDbContext())
+                {
+                    listCategories = context.Categories.ToList();
+
+                }
+            }
+           catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return listCategories;
+            
+        }
+    }
+}
---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using BusinessObjects;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        public void DeleteProduct(Product p) => ProductDAO.DeleteProduct(p);
+
+        public List<Category> GetCategories() => CategoryDAO.GetCategories();
+
+        public Product GetProductByid(int id) => ProductDAO.FindProductById(id);
+
+
+        public List<Product> GetProducts() => ProductDAO.GetProducts();
+
+        public void SaveProduct(Product p) => ProductDAO.SaveProduct(p);
+
+        public void UpdateProduct(Product p) => ProductDAO.UpdateProduct(p);
+    }
+}
